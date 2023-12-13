6d6bb1cf04180eebc110d10fcc907b2e49de79a4
wltjd1688 <wltjd1688@gmail.com>
2023-12-14 03:16:34 +0900

    [updata] ten- 테이블 사이즈 thin- 아이콘 변경

--- src/app/components/ThinSidebar.tsx
+++ src/app/components/ThinSidebar.tsx
@@ -53,20 +53,20 @@ const ThinSidebar: React.FC = () => {
     <div className={`thinSidebar ${isDarkMode ? 'darkMode' : ''}`}>
       <div className='thinIconBox'>
         <div className=" group">
-          <Tooltip text="Info">
-            <div className={`${isSelectedIcon('detail')}`} onClick={() => handleIconClick('detail')}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill={`${isSelectedIcon('detail')}`} className="bi bi-info-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+          <Tooltip text="List">
+            <div className={`${isSelectedIcon('list')}`} onClick={() => handleIconClick('list')}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill={`${isSelectedIcon('list')}`} className="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+                <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
+                <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
               </svg>
             </div>
           </Tooltip>
         </div>
         <div className=" group">
-          <Tooltip text="List">
-            <div className={`${isSelectedIcon('list')}`} onClick={() => handleIconClick('list')}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill={`${isSelectedIcon('list')}`} className="bi bi-list-columns-reverse" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M0 .5A.5.5 0 0 1 .5 0h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 .5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10A.5.5 0 0 1 4 .5m-4 2A.5.5 0 0 1 .5 2h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 4h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 6h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 8h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
+          <Tooltip text="Info">
+            <div className={`${isSelectedIcon('detail')}`} onClick={() => handleIconClick('detail')}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill={`${isSelectedIcon('detail')}`} className="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
               </svg>
             </div>
           </Tooltip>
--- src/app/components/etc/Tendisaster.tsx
+++ src/app/components/etc/Tendisaster.tsx
@@ -69,14 +69,14 @@ export default function TenDisaster() {
       </div>
       <div className="relative">
         <table className="w-full text-left border-collapse">
-          <thead className="bg-gray-300 text-center">
-            <tr>
-              <th scope="col" className="py-3 px-2 border-b border-gray-300 w-8">Rank</th>
-              <th scope="col" className="py-3 px-2 border-b border-gray-300 w-15">Country</th>
-              <th scope="col" className="py-3 px-2 border-b border-gray-300 w-14">Type</th>
-              <th scope="col" className="py-3 px-2 border-b border-gray-300 flex-grow">Time</th>
-            </tr>
-          </thead>
+        <thead className="bg-gray-300 text-center">
+          <tr>
+            <th scope="col" className="py-3 px-2 border-b border-gray-300 w-8">Rank</th>
+            <th scope="col" className="py-3 px-2 border-b border-gray-300 w-40 ellipsis">Country</th>
+            <th scope="col" className="py-3 px-2 border-b border-gray-300 ellipsis">Type</th>
+            <th scope="col" className="py-3 px-2 border-b border-gray-300 flex-grow ellipsis">Time</th>
+          </tr>
+        </thead>
           <tbody>
             {sortedDisasters.map((item, index) => (
               <>
@@ -86,7 +86,7 @@ export default function TenDisaster() {
                   className={`cursor-pointer ${item.dStatus === 'real-time' ? 'text-blue-600 font-semibold' : 'text-gray-700'} hover:bg-gray-200 transition duration-150 ease-in-out border-b border-gray-300`}
                 >
                   <td className="py-2 px-2 text-center">{getRank(index)}</td>
-                  <td className="py-2 px-2 text-center">{item.dCountry}</td>
+                  <td className="py-2 px-2 text-center ">{item.dCountry}</td>
                   <td className="py-2 px-2 text-center">{item.dType}</td>
                   <td className="py-2 px-2 text-center">{formatDateTime(item.dDate)}</td>
                 </tr>
@@ -96,7 +96,7 @@ export default function TenDisaster() {
                       <div className="flex justify-between items-center">
                         <span>{item.dTitle || 'No Title'}</span>
                         <button 
-                          className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                          className="inline-flex text-white bg-sky-400 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                           onClick={() => handleDetailView(item)}
                         >
                           View Details
@@ -119,13 +119,13 @@ export default function TenDisaster() {
         pageRangeDisplayed={2}
         onPageChange={handlePageClick}
         containerClassName={"pagination flex justify-center mt-4 items-center"}
-        activeClassName={"active bg-indigo-500 text-white px-3 py-2 rounded-full mx-1"}
+        activeClassName={"active bg-gray-300 text-white rounded-full px-4 py-2"}
         disabledClassName={"disabled text-gray-500"}
         previousClassName={"previous mx-1"}
         nextClassName={"next mx-1"}
         breakClassName={"break mx-1"}
         pageClassName={"page mx-1"}
-        pageLinkClassName={"page-link border border-gray-300 rounded-full hover:bg-gray-200"}
+        pageLinkClassName={"page-link border border-gray-300 rounded-full hover:bg-gray-200 hover:px-2 hover:py-2"}
         previousLinkClassName={"previous-link px-1 py-2 border border-gray-300 rounded-full hover:bg-gray-200"}
         nextLinkClassName={"next-link px-3 py-2 border border-gray-300 rounded-full hover:bg-gray-200"}
         breakLinkClassName={"break-link px-3 py-2"}
